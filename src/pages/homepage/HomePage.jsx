859c31c105a63b3430fcee2c9480570e9697fe2a
Kellan Benn <kbenn@imaginuity.com>
2020-05-27 13:12:42 -0500

    broke homepage into smaller components, finished styling menu-item component

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Directory from '../../components/directory/directory';
+
+import './HomePage.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
